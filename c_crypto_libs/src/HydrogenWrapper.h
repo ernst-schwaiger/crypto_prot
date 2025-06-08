a3229728b74af82a9a7fff3060184d1c9cebfd52
Ernst Schwaiger <ernst.schwaiger@chello.at>
2025-06-08 10:42:09 +0200

    fix warnings/errors issued by clang

--- c_crypto_libs/src/HydrogenWrapper.h
+++ c_crypto_libs/src/HydrogenWrapper.h
@@ -16,7 +16,7 @@ public:
         return std::unique_ptr<HydrogenWrapper>(new HydrogenWrapper());        
     }
 
-    virtual uint8_t getId() const
+    virtual uint8_t getId() const override
     {
         return WRAPPER_ID_LIBHYDROGEN; // ...and the LibHydrogenWrapper shall return 1;
     }
@@ -28,10 +28,10 @@ public:
     virtual payload_t finishDH(payload_t &remote_key) override;
 
     // generates IV, encrypts using the symmetric key and returns IV and ciphertext
-    virtual std::pair<payload_t, payload_t> encrypt(std::string const &plainText, payload_t const &symmKey) const;
+    virtual std::pair<payload_t, payload_t> encrypt(std::string const &plainText, payload_t const &symmKey) const override;
 
     // decrypts from IV, symmetric key and ciphertext
-    virtual std::string decrypt(std::pair<payload_t, payload_t> const &ivAndCipherText, payload_t const &symmKey) const;    
+    virtual std::string decrypt(std::pair<payload_t, payload_t> const &ivAndCipherText, payload_t const &symmKey) const override;    
 
     virtual ~HydrogenWrapper()
     {
